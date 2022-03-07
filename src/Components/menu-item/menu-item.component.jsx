b809f7a7d952385b45a619e4d9684ed621b99ce2
Nitin Yadav~ <nitin.yadav966@hotmail.com>
2022-03-07 20:07:53 +0530

    directory added

--- src/Components/menu-item/menu-item.component.jsx
+++ src/Components/menu-item/menu-item.component.jsx
@@ -1,10 +1,14 @@
 import React from "react";
 
-const MenuItem = () => (
+import "./menu-item.styles.scss";
+
+const MenuItem = ({ title }) => (
   <div className="menu-item">
     <div className="content">
-      <h1 className="title"> JACKETS</h1>
+      <h1 className="title"> {title}</h1>
       <span className="subtitle"> SHOP NOW </span>
     </div>
   </div>
 );
+
+export default MenuItem;
